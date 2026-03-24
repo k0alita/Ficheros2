14d071b8b2a83f4a96557f5337529687891ac865
k0alita <alejandrohernandossan@gmail.com>
2026-03-24 12:59:27 +0100

    ejercicios resueltos

--- src/Boletin1/Ejercicio5/Ejercicio5.java
+++ src/Boletin1/Ejercicio5/Ejercicio5.java
@@ -2,12 +2,18 @@ package Boletin1.Ejercicio5;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
 
 public class Ejercicio5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String nombre = sc.nextLine();
 
+
+    }
+
+    public static void crearDirectorio(String nombreDelDirectorio) {
+        Path directorio = Path.of("src", "Boletin1", "Ejercicio5", "salidasEjercicio5");
     }
 }
